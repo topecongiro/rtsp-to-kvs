0b48ab412b25fbaa0d90746f94890475942cd28a
Seiichi Uchida <topecongiro@fastmail.com>
2022-04-09 21:42:25 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,42 @@
-use clap::{ArgEnum, Parser};
+use clap::{Args, Parser, Subcommand};
 use gst::prelude::*;
 
 #[derive(Debug, Parser)]
-struct Args {
+#[clap(author, version, about)]
+#[clap(infer_subcommands = true)]
+struct Cli {
+    /// The kind of sink to use.
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+impl Cli {
+    fn rtsp_config(&self) -> &RtspConfig {
+        match &self.command {
+            Commands::PlayBack { rtsp_config: rtsp_setting } => rtsp_setting,
+            Commands::Kvs { rtsp_config: rtsp_setting, .. } => rtsp_setting,
+        }
+    }
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Playback the RTSP stream (video only)
+    PlayBack {
+        #[clap(flatten)]
+        rtsp_config: RtspConfig,
+    },
+    /// Stream the RTSP stream to Kinesis Video Stream
+    Kvs {
+        #[clap(flatten)]
+        kvs_config: KvsConfig,
+        #[clap(flatten)]
+        rtsp_config: RtspConfig,
+    },
+}
+
+#[derive(Debug, Args)]
+struct RtspConfig {
     /// The RTSP url.
     #[clap(long)]
     url: String,
@@ -14,26 +48,22 @@ struct Args {
     /// The RTSP user password for authentication.
     #[clap(long)]
     password: Option<String>,
-
-
-    /// The kind of sink to use.
-    #[clap(arg_enum, default_value_t = SinkKind::default())]
-    sink: SinkKind,
 }
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
-enum SinkKind {
-    /// Playback the RTSP stream locally.
-    Playback,
-
-    /// Send the RTSP stream to KVS.
-    Kvs,
-}
-
-impl Default for SinkKind {
-    fn default() -> Self {
-        SinkKind::Playback
-    }
+#[derive(Debug, Args)]
+struct KvsConfig {
+        /// AWS access key.
+        #[clap(long)]
+        aws_access_key_id: String,
+        /// AWS secret key.
+        #[clap(long)]
+        aws_secret_key: String,
+        /// The stream name of Kinesis Video Stream
+        #[clap(long)]
+        stream_name: String,
+        /// AWS region.
+        #[clap(long)]
+        aws_region: Option<String>,
 }
 
 fn setup_playback(
@@ -65,26 +95,62 @@ fn setup_playback(
     Ok(())
 }
 
+fn setup_kvssink(
+    pipeline: &gst::Pipeline,
+    rtsp_source: &gst::Element,
+    rtph264depay: &gst::Element,
+    kvs_config: &KvsConfig,
+) -> anyhow::Result<()> {
+
+    let h264_parse = gst::ElementFactory::make("h264parse", Some("h264parse"))?;
+    let kvssink = gst::ElementFactory::make("kvssink", Some("kvssink"))?;
+    kvssink.try_set_property("access-key", kvs_config.aws_access_key_id.as_str())?;
+    kvssink.try_set_property("secret-key", kvs_config.aws_secret_key.as_str())?;
+    kvssink.try_set_property("stream-name", kvs_config.stream_name.as_str())?;
+    if let Some(ref aws_region) = kvs_config.aws_region {
+        kvssink.try_set_property("aws-region", aws_region)?;
+    }
+
+    pipeline.add_many(&[
+        rtsp_source,
+        rtph264depay,
+        &h264_parse,
+        &kvssink,
+    ])?;
+    gst::Element::link_many(&[
+        rtph264depay,
+        &h264_parse,
+        &kvssink,
+    ])?;
+
+    Ok(())
+}
+
+fn rtspsrc(rtsp_config: &RtspConfig) -> anyhow::Result<gst::Element> {
+    let rtsp_source = gst::ElementFactory::make("rtspsrc", Some("source"))?;
+    rtsp_source.try_set_property("location", rtsp_config.url.as_str())?;
+    if let Some(ref user_id) = rtsp_config.user_id {
+        rtsp_source.try_set_property("user-id", user_id)?;
+    }
+    if let Some(ref password) = rtsp_config.password {
+        rtsp_source.try_set_property("user-pw", password)?;
+    }
+    Ok(rtsp_source)
+}
+
 fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    let args = Args::parse();
+    let args = Cli::parse();
 
     gst::init()?;
 
     let pipeline = gst::Pipeline::new(Some("rtsp-to-kvs-pipeline"));
-    let rtsp_source = gst::ElementFactory::make("rtspsrc", Some("source"))?;
-    rtsp_source.try_set_property("location", args.url)?;
-    if let Some(user_id) = args.user_id {
-        rtsp_source.try_set_property("user-id", user_id)?;
-    }
-    if let Some(password) = args.password {
-        rtsp_source.try_set_property("user-pw", password)?;
-    }
+    let rtsp_source = rtspsrc(args.rtsp_config())?;
     let rtph264depay = gst::ElementFactory::make("rtph264depay", Some("rtph264depay"))?;
-    match args.sink {
-        SinkKind::Playback => setup_playback(&pipeline, &rtsp_source, &rtph264depay)?,
-        SinkKind::Kvs => todo!(),
+    match args.command {
+        Commands::PlayBack { .. } => setup_playback(&pipeline, &rtsp_source, &rtph264depay)?,
+        Commands::Kvs { kvs_config, .. } => setup_kvssink(&pipeline, &rtsp_source, &rtph264depay, &kvs_config)?,
     }
 
     rtsp_source.connect_pad_added(move |src, src_pad| {
